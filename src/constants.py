cb07e1b6ebc94f17d21c575a147b1ded20c2e3af
ashrafkhanofficial <muhammadashraf.offical2@gmail.com>
2026-07-12 23:52:19 +0500

    constants.py and baseline.yaml added

---
+++ src/constants.py
@@ -0,0 +1,26 @@
+# ==========================
+# Project Paths
+# ==========================
+
+DATA_DIR = "data"
+RAW_DATA_DIR = "data/raw"
+PROCESSED_DATA_DIR = "data/processed"
+DATASET_DIR = "data/raw/raw-img"
+
+CHECKPOINT_DIR = "checkpoints"
+RESULTS_DIR = "results"
+PLOTS_DIR = "results/plots"
+CONFUSION_MATRIX_DIR = "results/confusion_matrices"
+GRADCAM_DIR = "results/gradcam"
+
+# ==========================
+# Reproducibility
+# ==========================
+
+RANDOM_SEED = 42
+
+# ==========================
+# Dataset
+# ==========================
+
+NUM_CLASSES = 10
